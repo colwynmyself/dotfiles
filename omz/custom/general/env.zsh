8affd98d885c838c75a068e68b553acf40e7392b
Colwyn Fritze-Moor <colwyn@colwyn.me>
2025-10-26 21:42:36 -0700

    feat: add setup validation

--- omz/custom/general/env.zsh
+++ omz/custom/general/env.zsh
@@ -4,3 +4,9 @@ export REPOSITORY_DIR="$HOME/repositories"
 
 # GPG Git
 export GPG_TTY=$(tty)
+
+# Light setup validation
+if [[ ! -d "$REPOSITORY_DIR" ]]; then
+  # NOTE: This makes for a noisy p10k startup. That's fine because this is a problem
+  nicelog "[WARNING] $REPOSITORY_DIR isn't an existing directory. It should be where all repositories are cloned."
+fi
