570dd4212415e4bc958ea5b2c4322e0140a42bb0
Colwyn Fritze-Moor <colwyn@colwyn.me>
2026-04-22 13:56:13 -0700

    chore: disable various telemetry settings

--- omz/custom/general/env.zsh
+++ omz/custom/general/env.zsh
@@ -3,6 +3,13 @@ export PATH="$PATH:$HOME/bin"
 export REPOSITORY_DIR="$HOME/repositories"
 export EDITOR="vim"
 
+# Various tracking
+export GH_TELEMETRY=false
+export DO_NOT_TRACK=1
+export NEXT_TELEMETRY_DISABLED=1
+export SAM_CLI_TELEMETRY=0
+export CDK_DISABLE_CLI_TELEMETRY=true
+
 # GPG Git
 export GPG_TTY=$(tty)
 
